0963fb49fe642aed1b9de70c1e1c9cd77611724d
MK <test@just.edu.jo>
2022-10-25 08:30:09 +0300

    intail

---
+++ ConsoleApplication1/ConsoleApplication1/fun.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void main ()
+{
+	int age=90;
+	int year =2003;
+	double today =100.92;
+	cout <<"Age =:  "<<age<<endl;
+	cout <<"Year =:  "<<year<<endl;
+	cout <<"Today =:  "<<today<<endl;
+
+}
